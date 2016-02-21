b9c589a4ddbb3263efc298429fda82bfb37867ef
Jorge Aparicio <japaricious@gmail.com>
2016-02-21 09:47:40 -0500

    add osx build

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -3,10 +3,10 @@ set -ex
 mkdir deploy
 
 case $TARGET in
-  x86_64-unknown-linux-gnu)
-    cp target/release/hello deploy/$TARGET-hello
-    ;;
   x86_64-unknown-linux-musl)
     cp target/$TARGET/release/hello deploy/$TARGET-hello
+    ;;
+  *)
+    cp target/release/hello deploy/$TARGET-hello
     ;;
 esac
