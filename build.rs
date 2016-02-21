c7aa88f6826a3954055612e1971d059ac975be3c
Jorge Aparicio <japaricious@gmail.com>
2016-02-20 22:11:22 -0500

    initial commit

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+fn hello() -> String {
+    format!("fn main() {{ println!(\"{} says hello!\") }}", env::var("TARGET").unwrap())
+}
+
+fn main() {
+    File::create("src/main.rs").unwrap().write_all(hello().as_bytes());
+}
