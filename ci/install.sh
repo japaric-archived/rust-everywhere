143b9431aa8a1a56e4a1cf2bb43e083ffdc7560a
Jorge Aparicio <japaricious@gmail.com>
2016-02-21 17:17:48 -0500

    use arm-linux-gnueabihf std libs for armv7-linux-gnueabihf
    because there are no standard libraries for the latter target right now

--- ci/install.sh
+++ ci/install.sh
@@ -1,20 +1,27 @@
 set -ex
 
-# install standard libraries
+# install standard libraries needed for cross compilation
 case $TARGET in
   armv7-unknown-linux-gnueabihf | x86_64-unknown-linux-musl)
+    version=$(rustc -V | cut -d' ' -f2)
+
     if [ "$TARGET" = "armv7-unknown-linux-gnueabihf" ]; then
+      # There is no official standard libraries for this target, so we use the
+      # more generic ones
+      tarball=rust-std-${version}-arm-unknown-linux-gnueabihf
+
       arm-linux-gnueabihf-gcc -v
 
+      # configure the linker for cross compilation
       mkdir -p .cargo
       cat >.cargo/config <<EOF
 [target.$TARGET]
 linker = "arm-linux-gnueabihf-gcc"
 EOF
+    else
+      tarball=rust-std-${version}-${TARGET}
     fi
 
-    version=$(rustc -V | cut -d' ' -f2)
-    tarball=rust-std-${version}-${TARGET}
     curl -Os http://static.rust-lang.org/dist/${tarball}.tar.gz
     tar xzf ${tarball}.tar.gz
     ${tarball}/install.sh --prefix=$(rustc --print sysroot)
