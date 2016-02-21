bd60341330a2c543e6465c1ac326a21287a9b292
Jorge Aparicio <japaricious@gmail.com>
2016-02-21 00:04:18 -0500

    add package version to program output

--- build.rs
+++ build.rs
@@ -5,7 +5,9 @@ use std::fs::File;
 use std::io::Write;
 
 fn hello() -> String {
-    format!("fn main() {{ println!(\"{} says hello!\") }}", env::var("TARGET").unwrap())
+    format!("fn main() {{ println!(\"{}: {} says hello!\") }}",
+            env::var("CARGO_PKG_VERSION").unwrap(),
+            env::var("TARGET").unwrap())
 }
 
 fn main() {
