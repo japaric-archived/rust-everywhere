4a53ee0afb3b0b1c892efab4594d17d86204a9f8
Jorge Aparicio <japaricious@gmail.com>
2016-02-21 22:49:09 -0500

    travis: simplify logic by always passing --target to cargo

--- ci/script.sh
+++ ci/script.sh
@@ -1,25 +1,9 @@
 set -ex
 
-case $TARGET in
-  # Cross compilation
-  arm-unknown-linux-gnueabihf | \
-  i686-apple-darwin | \
-  i686-unknown-linux-gnu | \
-  x86_64-unknown-linux-musl)
-    cargo build --target $TARGET --verbose
-    if [ "$TARGET" != "arm-unknown-linux-gnueabihf" ]; then
-      cargo run --target $TARGET
-      cargo test --target $TARGET
-    fi
-    cargo build --target $TARGET --release
-    file target/$TARGET/release/hello
-    ;;
-  # Native build
-  *)
-    cargo build --verbose
-    cargo run
-    cargo test
-    cargo build --release
-    file target/release/hello
-    ;;
-esac
+cargo build --target $TARGET --verbose
+if [ "$TARGET" != "arm-unknown-linux-gnueabihf" ]; then
+  cargo run --target $TARGET
+  cargo test --target $TARGET
+fi
+cargo build --target $TARGET --release
+file target/$TARGET/release/hello
