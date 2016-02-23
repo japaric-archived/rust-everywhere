897473a84a591438b62d5003222fa5c33a91731f
Jorge Aparicio <japaricious@gmail.com>
2016-02-22 22:19:46 -0500

    arm-linux-gnueabihf: use qemu user emulation to run tests
    cc #12

--- ci/script.sh
+++ ci/script.sh
@@ -7,13 +7,28 @@ set -ex
 # to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
 # `before_deploy`/packaging phase
 
-cargo build --target $TARGET --verbose
+case $TARGET in
+  # use an emulator to run the cross compiled binaries
+  arm-unknown-linux-gnueabihf)
+    # build tests but don't run them
+    cargo test --target $TARGET --no-run
 
-# can't run binaries for this target
-if [ "$TARGET" != "arm-unknown-linux-gnueabihf" ]; then
-  cargo run --target $TARGET
-  cargo test --target $TARGET
-fi
+    # run tests in emulator
+    find target/$TARGET/debug -maxdepth 1 -executable -type f | \
+      xargs qemu-arm -L /usr/arm-linux-gnueabihf
+
+    # build the main executable
+    cargo build --target $TARGET
+
+    # run the main executable using the emulator
+    qemu-arm -L /usr/arm-linux-gnueabihf target/$TARGET/debug/hello
+    ;;
+  *)
+    cargo build --target $TARGET --verbose
+    cargo run --target $TARGET
+    cargo test --target $TARGET
+    ;;
+esac
 
 cargo build --target $TARGET --release
 
