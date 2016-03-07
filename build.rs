87f2f922a57ba92b3247528bf6e4a6b5180bb1d4
Jorge Aparicio <japaricious@gmail.com>
2016-03-07 15:15:17 -0500

    add RELEASE_CHANNEL variable to let the user pick which channel to use for releases
    release artifacts are only generated on $RELEASE_CHANNEL and only when a deployment is due.
    
    The `hello` app nows print the rust version/channel it was compiled with; to aid debugging.

--- build.rs
+++ build.rs
@@ -1,13 +1,19 @@
 #![deny(warnings)]
 
+extern crate rustc_version;
+
 use std::env;
 use std::fs::File;
 use std::io::Write;
 
 fn hello() -> String {
-    format!("fn main() {{ println!(\"{}: {} says hello!\") }}\n#[test] fn ok() {{ assert!(true) }}",
+    let meta = rustc_version::version_meta();
+
+    format!("fn main() {{ println!(\"{}: {} says hello!\nCompiled with rust-{} ({:?} channel)\") }}\n#[test] fn ok() {{ assert!(true) }}",
             env::var("CARGO_PKG_VERSION").unwrap(),
-            env::var("TARGET").unwrap())
+            env::var("TARGET").unwrap(),
+            meta.semver,
+            meta.channel)
 }
 
 fn main() {
