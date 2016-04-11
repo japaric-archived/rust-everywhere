92648b8b618b796ea77da59591f8fed3c9cbc84a
Jorge Aparicio <japaricious@gmail.com>
2016-04-11 16:47:02 -0500

    push -> pushd

--- ci/install.sh
+++ ci/install.sh
@@ -18,7 +18,7 @@ mktempd() {
 install_rustup() {
   local td=$(mktempd)
 
-  push $td
+  pushd $td
   curl -O https://static.rust-lang.org/rustup/dist/$host/rustup-setup
   chmod +x rustup-setup
   ./rustup-setup -y
